b70dc538903b06706eaaa84219c2093c7fc26ae9
ccyx <702609592@qq.com>
2023-02-06 19:03:56 +0800

    source

--- packages/page-header/src/main.vue
+++ packages/page-header/src/main.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="el-page-header">
-    <div class="el-page-header__left" @click="$emit('back')">
+    <div class="el-page-header__left" @click="$emit('back')" v-if="!titleRight">
       <i class="el-icon-back"></i>
       <div class="el-page-header__title">
         <slot name="title">{{ title }}</slot>
       </div>
+    </div>
+    <div class="el-page-header__right" @click="$emit('back')" v-else>
+      <div class="el-page-header__title">
+        <slot name="title">{{ title }}</slot>
+      </div>
+      <i class="el-icon-arrow-right"></i>
     </div>
     <div class="el-page-header__content">
       <slot name="content">{{ content }}</slot>
@@ -23,6 +29,10 @@ export default {
       default() {
         return t('el.pageHeader.title');
       }
+    },
+    titleRight: {
+      type: Boolean,
+      default: false
     },
     content: String
   }
