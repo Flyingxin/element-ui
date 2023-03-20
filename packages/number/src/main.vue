c9444462f64a8ebe74785069d6c41bfcf9639123
ccyx <702609592@qq.com>
2023-03-20 22:39:24 +0800

    source

--- packages/number/src/main.vue
+++ packages/number/src/main.vue
@@ -1,16 +1,5 @@
 <template>
-  <div class="el-number"
-  :class="[
-    color? 'el-number--' + color : '',
-    fontSize? 'el-number--' + fontSize : '',
-    {
-      'is-bold': bold,
-      'is-hover': hover,
-      'is-border': border
-    }
-  ]"
-  :style="style">
-  <i :class="icon" :style='iconStyle' v-if="icon"></i>
+  <div class="el-number">
     {{ number }}
   </div>
 </template>
@@ -26,46 +15,10 @@ export default {
     delay: {
       type: Number,
       require: true
-    },
-    size: String,
-    bold: Boolean,
-    color: String,
-    hover: Boolean,
-    icon: {
-      type: String,
-      default: ''
-    },
-    iconColor: {
-      type: String,
-      default: ''
-    },
-    iconSize: Number,
-    border: Boolean
-  },
-  data() {
-    return {
-      number: 0,
-      offsetHeight: ''
-    };
-  },
-  computed: {
-    fontSize() {
-      return this.size || '';
-    },
-    style() {
-      return {
-        'line-height': `${this.offsetHeight}px`
-      };
-    },
-    iconStyle() {
-      return {
-        color: this.iconColor,
-        'font-size': `${this.iconSize}px`
-      };
     }
   },
-  methods: {
-    scrollNumber() {
+  computed: {
+    number() {
       let index = 0;
       let start = 0;
       let steps = parseInt(this.delay / 200, 0);
@@ -76,14 +29,10 @@ export default {
         this.number = start;
         if (index === steps) {
           clearInterval(timer);
-          this.number = start + (this.value % step);
+          return start + (this.value % step);
         }
       }, steps);
     }
-  },
-  mounted() {
-    this.scrollNumber();
-    this.offsetHeight = this.$el.offsetHeight;
   }
 };
 </script>
